0485cb2b0d0b2c07c029650a6ad796313887cc85
deepak <deepak@socash.sg>
2021-07-15 23:41:08 +0530

    Fix compilation error

--- api/src/main/java/io/cyberia/example/api/PersonsController.java
+++ api/src/main/java/io/cyberia/example/api/PersonsController.java
@@ -1,13 +1,14 @@
 package io.cyberia.example.api;
 
-import com.sun.tools.javac.util.List;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.mediatype.hal.HalModelBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static io.cyberia.example.api.MediaTypes.VERSION_1;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -19,8 +20,7 @@ public class PersonsController {
 
   @GetMapping
   public RepresentationModel<PersonsModel> getAll() {
-    List<PersonModel> persons = List.of(new PersonDTO("Me", 22), new PersonDTO("You", 25))
-        .map(p -> new PersonModel(p.getName(), p.getAge()));
+    List<PersonModel> persons = Stream.of(new PersonDTO("Me", 22), new PersonDTO("You", 25)).map(p -> new PersonModel(p.getName(), p.getAge())).collect(Collectors.toList());
 
     PersonsModel personsModel = new PersonsModel(persons.size(), persons.size());
     return HalModelBuilder
